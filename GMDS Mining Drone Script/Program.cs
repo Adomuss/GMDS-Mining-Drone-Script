9410feffdbcb662130998226c2ba94a89bc0c502
Adomuss <adomusmusic@gmail.com>
2026-09-03 12:30:02 +0100

    fantastic update comms alignment

--- GMDS Mining Drone Script/Program.cs
+++ GMDS Mining Drone Script/Program.cs
@@ -5610,7 +5610,7 @@ namespace IngameScript
 
             if (pinged)
             {
-                const string baseFormat = "{0}:{1}:{2}:{3}:{4}:{5}:{6}:{7}:{8}:{9}:{10}:{11}:{12}:{13}:{14}:{15}:{16}:{17}:{18}:{19}:{20}:{21}:{22}:{23}:{24}";
+                const string baseFormat = "{0}:{1}:{2}:{3}:{4}:{5}:{6}:{7}:{8}:{9}:{10}:{11}:{12}:{13}:{14}:{15}:{16}:{17}:{18}:{19}:{20}:{21}:{22}:{23}:{24}:";
                 sb.Clear().EnsureCapacity(128);
                 sb.AppendFormat(baseFormat, D_I_N,
                     droneDamageStatus, tunnelSequenceFinished, droneStatusOutput,
